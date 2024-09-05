c72a3d75dbe09dcd3b695ec03f5445a0a8f4fa57
yogagv <yoganandhan465@gmail.com>
2024-09-05 18:21:08 +0530

    Link_Update

--- src/Components/routes/Routing.jsx
+++ src/Components/routes/Routing.jsx
@@ -5,16 +5,20 @@ import About from '../About/About'
 import Services from '../Services/Services'
 import Events from '../Events/Events'
 import Gallery from '../Gallery/Gallery'
+import Testimonials from '../Testimonials/Testimonials'
+import Footer from '../Footer/Footer'
 
 const Routing = () => {
   return (
     <div>
         <Routes>
             <Route path='/' element={<Home/>}/>
-            <Route path='/#about' element={<About/>}/>
-            <Route path='/#services' element={<Services/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/services' element={<Services/>}/>
             <Route path='/events' element={<Events/>}/>
             <Route path='/gallery' element={<Gallery/>}/>
+            <Route path='/testimonials' element={<Testimonials/>}/>
+            <Route path='/contact' element={<Footer/>}/>
         </Routes>
     </div>
   )
